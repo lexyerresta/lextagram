a2b4243f72b09016d7decfc25840295d6a1efa31
lexyerresta <errestlegends@gmail.com>
2023-12-21 13:59:47 +0800

    Setup Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
+import SigninForms from './_auth/forms/SigninForms';
+import SignupForms from './_auth/forms/SignupForms';
+import AuthLayout from './_auth/forms/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
 import './globals.css';
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello Lextagram!
-        </h1>
+        <main className='flex h-screen'>
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/sign-in" element={<SigninForms />} />
+                    <Route path="/sign-up" element={<SignupForms />} />
+                </Route>
+
+                {/* private routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
     )
 }
 

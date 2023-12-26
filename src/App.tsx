3f44b1b530f813ef80da15f81d26235acef8445c
lexyerresta <errestlegends@gmail.com>
2023-12-26 17:49:40 +0800

    Routing Left Side Bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SigninForms from './_auth/forms/SigninForms';
 import SignupForms from './_auth/forms/SignupForms';
 import AuthLayout from './_auth/forms/AuthLayout';
 import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
@@ -21,6 +21,14 @@ const App = () => {
                 {/* private routes */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id/*" element={<Profile />} />
+                    <Route path="/update-profile/:id" element={<UpdateProfile />} />
                 </Route>
             </Routes>
             

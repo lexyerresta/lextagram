66f50fecc404922fe7c5dff49d4dc5434ffc3d16
lexyerresta <errestlegends@gmail.com>
2024-01-03 19:14:30 +0800

    Refactor PostCard component and update multiFormatDateString function

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,8 +1,9 @@
-import { useUserContext } from "@/context/AuthContext";
-import { formatDate } from "@/lib/utils";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+
 import PostStats from "./PostStats";
+import { multiFormatDateString } from "@/lib/utils";
+import { useUserContext } from "@/context/AuthContext";
 
 type PostCardProps = {
     post: Models.Document;
@@ -19,9 +20,12 @@ const PostCard = ({ post }: PostCardProps) => {
                 <div className="flex-items-center gap-3">
                     <Link to={`/profile/${post.creator.$id}`}>
                         <img
-                            src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'}
+                            src={
+                                post.creator?.imageUrl ||
+                                "/assets/icons/profile-placeholder.svg"
+                            }
                             alt="creator"
-                            className="rounded-full w-12 lg:h-12"
+                            className="w-12 lg:h-12 rounded-full "
                         />
                     </Link>
 
@@ -31,9 +35,9 @@ const PostCard = ({ post }: PostCardProps) => {
                         </p>
                         <div className="flex-center gap-2 text-light-3">
                             <p className="subtle-semibold lg:small-regular">
-                                {formatDate(post.$createdAt)}
+                                {multiFormatDateString(post.$createdAt)}
                             </p>
-                            -
+                            •
                             <p className="subtle-semibold lg:small-regular">
                                 {post.location}
                             </p>
@@ -43,33 +47,38 @@ const PostCard = ({ post }: PostCardProps) => {
 
                 <Link
                     to={`/update-post/${post.$id}`}
-                    className={`${user.id !== post.creator.$id && "hidden"}`}
-                >
-                    <img src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
+                    className={`${user.id !== post.creator.$id && "hidden"}`}>
+                    <img
+                        src="/assets/icons/edit.svg"
+                        alt="edit"
+                        width={20}
+                        height={20}
+                    />
                 </Link>
             </div>
+
             <Link to={`/posts/${post.$id}`}>
                 <div className="small-medium lg:base-medium py-5">
                     <p>{post.caption}</p>
                     <ul className="flex gap-1 mt-2">
-                        {post.tags.map((tag: string) => (
-                            <li key={tag} className="text-light-3">
+                        {post.tags.map((tag: string, index: string) => (
+                            <li key={`${tag}${index}`} className="text-light-3 small-regular">
                                 #{tag}
                             </li>
                         ))}
                     </ul>
                 </div>
 
-                <img 
-                    src={post.imageUrl || '/assets/icons/profile-placeholder.svg'}
-                    className="post-card_img"
+                <img
+                    src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
                     alt="post image"
+                    className="post-card_img"
                 />
             </Link>
 
             <PostStats post={post} userId={user.id} />
         </div>
-    )
-}
+    );
+};
 
-export default PostCard
+export default PostCard;
